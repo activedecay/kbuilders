ce3875466f2b84b4d12c5e7b889f77a010bd45bb
Aaron Sarazan <aaron@levelmoney.com>
2015-03-24 21:57:20 -0700

    Fix test resource import

--- src/test/kotlin/com/levelmoney/kbuilders/test/Tests.kt
+++ src/test/kotlin/com/levelmoney/kbuilders/test/Tests.kt
@@ -1,6 +1,9 @@
 package com.levelmoney.kbuilders.test
 
+import com.levelmoney.wire.kbuilders.generate
 import org.junit.Test
+import java.io.File
+import java.net.URL
 
 /**
  * Created by Aaron Sarazan on 3/24/15
@@ -9,8 +12,12 @@ import org.junit.Test
 
 public class Tests {
 
+    private fun getResource(path: String): URL? = javaClass.getClassLoader().getResource(path)
+
     Test fun testSomething() {
-        
+        val address = getResource("proto/Address.txt")!!
+        val parsed = generate("com.levelmoney", listOf(File(address.getFile())))
+        // nothing
     }
 
 }
