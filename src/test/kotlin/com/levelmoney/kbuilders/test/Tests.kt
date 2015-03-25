5e8aac257544b2c61c6bb4e48b2c6eebb03f285a
Aaron Sarazan <aaron@levelmoney.com>
2015-03-24 21:47:33 -0700

    Basic test framework

---
+++ src/test/kotlin/com/levelmoney/kbuilders/test/Tests.kt
@@ -0,0 +1,16 @@
+package com.levelmoney.kbuilders.test
+
+import org.junit.Test
+
+/**
+ * Created by Aaron Sarazan on 3/24/15
+ * Copyright(c) 2015 Level, Inc.
+ */
+
+public class Tests {
+
+    Test fun testSomething() {
+        
+    }
+
+}
