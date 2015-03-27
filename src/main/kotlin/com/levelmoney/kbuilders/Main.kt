b1fbc39575875e73b1efcb79f285f4f7dd83d596
Aaron Sarazan <aaron@levelmoney.com>
2015-03-26 18:57:37 -0700

    Hot

--- src/main/kotlin/com/levelmoney/kbuilders/Main.kt
+++ src/main/kotlin/com/levelmoney/kbuilders/Main.kt
@@ -1,11 +1,11 @@
 package com.levelmoney.kbuilders
 
 import com.github.javaparser.JavaParser
-import com.github.javaparser.ast.PackageDeclaration
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration
-import com.github.javaparser.ast.body.MethodDeclaration
-import com.github.javaparser.ast.visitor.VoidVisitorAdapter
-import com.levelmoney.kbuilders.javaparser.extensions.*
+import com.levelmoney.kbuilders.javaparser.extensions.getBuilders
+import com.levelmoney.kbuilders.javaparser.extensions.getMethodStrings
+import com.levelmoney.kbuilders.javaparser.extensions.getRequiredImports
+import joptsimple.OptionParser
+import joptsimple.OptionSet
 import java.io.File
 
 /**
@@ -13,24 +13,53 @@ import java.io.File
  * Copyright(c) 2015 Level, Inc.
  */
 
+/**
+ * java -jar kbuilder.jar --proto_root=<dir> --kotlin_root=<dir>
+ */
 fun main(args : Array<String>) {
-    val file = args[0]
-    print(generateString(File(file)))
+    if (args.size() < 2) {
+        println("Usage: java -jar kbuilder.jar --proto_root=<dir> --kotlin_root=<dir>")
+        return
+    }
+    val parser = OptionParser()
+    parser.accepts("proto_root").withRequiredArg()
+    parser.accepts("kotlin_root").withRequiredArg()
+    val options = parser.parse(*args)
+
+    val protoRoot = options.valueOf("proto_root").toString()
+    val kotlinRoot = options.valueOf("kotlin_root").toString()
+
+    val dir = File(protoRoot)
+    dir.recurse {
+        if (it.isFile()) {
+            val pkgAndText = generatePackageAndText(it)
+            if (pkgAndText != null) {
+                val name = it.getName().replace("."+it.extension, ".kt")
+                val (pkg, text) = pkgAndText
+                val dest = File(kotlinRoot, pkg.split("\\.").join("/") + "/" + name)
+                dest.getParentFile().mkdirs()
+                dest.writeText(text)
+            }
+        }
+    }
 }
 
 
 /**
  * Generates a full kotlin file from the provided java files.
  */
-public fun generateString(file: File): String {
+public fun generatePackageAndText(file: File): Pair<String, String>? {
     val cu = JavaParser.parse(file)
     val pakage = cu.getPackage().getName().toString()
     val imports = cu.getRequiredImports()
+    val builders = cu.getBuilders()
+    if (builders.isEmpty()) return null
     val methods = cu.getBuilders().flatMap { it.getMethodStrings() }
-    return """package $pakage
+    return Pair(pakage,
+"""package $pakage
 
 ${imports.map{ "import " + it }.join("\n")}
 
 ${methods.join("\n")}
-"""
+""")
 }
